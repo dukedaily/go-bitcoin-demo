80bb288796aec0400e22b4386e450caed5676486
duke.du <duke@MacBook-Pro-2.local>
2019-06-26 21:47:17 +0800

    day1

---
+++ blockchain-v2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//打印区块链
+func main() {
+	bc := NewBlockChain()
+	//变量区块数据
+	time.Sleep(1 * time.Second)
+
+	bc.AddBlock("26号btc暴涨20%")
+	time.Sleep(1 * time.Second)
+
+	bc.AddBlock("27号btc暴涨10%")
+	time.Sleep(1 * time.Second)
+
+	for i, block := range bc.Blocks {
+		fmt.Printf("\n+++++++++ 当前区块高度: %d ++++++++++\n", i)
+		fmt.Printf("Version : %d\n", block.Version)
+		fmt.Printf("PrevHash : %x\n", block.PrevHash)
+		fmt.Printf("MerkleRoot : %x\n", block.MerkleRoot)
+		fmt.Printf("TimeStamp : %d\n", block.TimeStamp)
+		fmt.Printf("Bits : %d\n", block.Bits)
+		fmt.Printf("Nonce : %d\n", block.Nonce)
+		fmt.Printf("Hash : %x\n", block.Hash)
+		fmt.Printf("Data : %s\n", block.Data)
+	}
+}
---
+++ test/02-bytesjoin.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	var nums []int
+
+	nums = []int{1, 2, 3, 4}
+
+	nums2 := [][]int{
+		[]int{1, 2, 3},
+		[]int{4, 5, 6},
+	}
+
+	tmp := [][]byte{
+		[]byte("hello"),
+		[]byte("world"),
+	}
+}
